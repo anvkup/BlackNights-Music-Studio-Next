e2f8c7a80deb7625959c78693a7a53254b10b034
Anvesh Kumar <55266595+anvkup@users.noreply.github.com>
2024-04-18 18:10:38 +0000

    vercel-analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "./style.css";
+import { Analytics } from '@vercel/analytics/react';
 
 import React from "react";
 export default function MyApp({
   Component: Component,
   pageProps: pageProps
 }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
